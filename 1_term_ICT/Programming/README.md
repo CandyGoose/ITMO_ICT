0d3cb3cee255995d4e02099ca5eaf9a8fc3514ab
VeraKasianenko <verakasianenko02@gmail.com>
2023-05-16 11:06:46 +0300

    ICT

---
+++ 1_term_ICT/Programming/README.md
@@ -0,0 +1,1 @@
+# [Ссылка на все материалы лекций](https://drive.google.com/drive/folders/12GicINdAM_iMaAf5PCk1r3xmI87ZXkg7)
---
+++ 1_term_ICT/Programming/prog_1/cher 6 - 4.1.py
@@ -0,0 +1,20 @@
+import turtle
+
+'''
+Создайте программу для реализации «черепашьей графики». Используйте модуль turtle.
+Заставьте Вашу черепаху пройти замкнутый многоугольный контур (с числом углов больше четырех), вернувшись в 
+начальную точку.
+'''
+
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
+turtle.forward(50)
+turtle.right(60)
